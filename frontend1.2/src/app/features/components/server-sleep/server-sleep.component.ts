4df3e1a064182d0a5d65f2742558d014a6bf5ba7
412045-Carabelli <412045@tecnicatura.frc.utn.edu.ar>
2025-10-24 19:47:08 -0300

    waking up server

---
+++ frontend1.2/src/app/features/components/server-sleep/server-sleep.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+import { NgIf } from '@angular/common';
+
+@Component({
+  selector: 'app-server-sleep',
+  standalone: true,
+  templateUrl: './server-sleep.component.html',
+  styleUrls: ['./server-sleep.component.css']
+})
+export class ServerSleepComponent {
+}
